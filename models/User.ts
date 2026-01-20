11ffed04fbf17ba160d753ca5c86c489744499d6
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-01-20 07:07:08 +0530

    Design models for Nextjs application

---
+++ models/User.ts
@@ -0,0 +1,24 @@
+import mongoose, {Schema,model,models} from "mongoose";
+import bcrypt from "bcryptjs"
+
+export interface IUser{
+    email:string,
+    password:string,
+    _id?:mongoose.Types.ObjectId,
+    createdAt?:Date,
+    updatedAt?:Date
+}
+const userSchema=new Schema<IUser>({
+    email:{type:String, required:true,unique:true},
+    password:{type:String,required:true}
+},
+{
+    timestamps:true
+})
+userSchema.pre('save',async function(){
+    if(this.isModified('password')){
+        this.password=await bcrypt.hash(this.password,10)
+    }
+})
+const User=models?.User || model<IUser>('User',userSchema)
+export default User
