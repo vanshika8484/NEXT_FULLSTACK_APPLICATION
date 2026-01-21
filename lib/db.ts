280d5f71a9730fdf3a3ece42dfc48d78b96c8e18
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-01-21 06:57:24 +0530

    conect with database in nextjs

---
+++ lib/db.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+const MONGODB_URI=process.env.MONGODB_URI!
+
+if(!MONGODB_URI){
+    throw new Error('Please provide MONGODB_URI in the environment variables')
+}
+let cached=global.mongoose
+if(!cached){
+    cached=global.mongoose= {conn:null,promise:null}
+}
+export async function connectToDatabase(){
+    if(cached.conn){
+        return cached.conn
+    }
+    if(!cached.promise){
+        const opts={
+            bufferCommands:true,
+            maxPoolSize:10
+        }
+        cached.promise=mongoose
+        .connect(MONGODB_URI,opts)
+        .then(()=>mongoose.connection)
+    }
+    try{
+        cached.conn = await cached.promise
+    }
+    catch(e){
+        cached.promise = null
+    }
+    return cached.conn
+}
